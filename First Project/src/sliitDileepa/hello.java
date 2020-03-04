8e2cc217e52a91584c4019f49d2557d03739b482
dileepapavel <it18107210@my.sliit.lk>
2020-03-04 05:54:56 -0800

    this is the first java

---
+++ First Project/src/sliitDileepa/hello.java
@@ -0,0 +1,10 @@
+package sliitDileepa;
+
+public class hello {
+	
+	
+	public static void main(String [] args) {
+		System.out.println("hello world this is my first java project");
+		
+	}
+}
---
+++ First Project/src/sliitDileepa/person.java
@@ -0,0 +1,10 @@
+package sliitDileepa;
+
+public class person {
+	
+	
+	public static void main(String [] args) {
+		System.out.println("hello world this is my first java project");
+		
+	}
+}
